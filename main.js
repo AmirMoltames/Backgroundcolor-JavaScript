f0c4cb853dc0de540917614b248358dd4158d476
AmirMoltames <74910734+AmirMoltames@users.noreply.github.com>
2021-04-24 01:58:08 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+function changePageBackground() {
+  let inputElement = document.getElementById("pageBackgroundInput");
+  let backgroundColor = inputElement.value;
+  document.body.style.backgroundColor = backgroundColor;
+}
+
+function changeContentBackground() {
+  console.log("changeContentBackground works!");
+  /*  
+     1. create a variable for your input field
+     2. get access to the correct input field and assign it to the variable you just created 
+    */
+
+  /* 
+    3. create a variable for the "div" of which you want to change the background  
+    4. get access to the "div" and assign it to the variable you just created
+    */
+
+  /* 
+    5. create a variable to store the backgroundColor
+    6. read the value from your input and assign it to the variable you just created
+  */
+
+  // use the style propert of your div element to change its backgroundColor
+}
+
+function changeMyName() {
+    /* 
+    Use the instructions from the other function as a guide.
+    Remember that the property to change the content is textContent
+    */
+  console.log("changeMyName works!");
+}
